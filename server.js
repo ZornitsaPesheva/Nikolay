e855ffb4ec8b071caceff314f1413248cbff5b61
Zornitsa Serbezova <zornitsa.pesheva@outlook.com>
2026-02-28 08:04:11 +0200

    server changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
     
 const mimeTypes = {
   '.html': 'text/html; charset=UTF-8',
